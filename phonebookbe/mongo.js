d0bfb3c74b935a5e21fd0480d47dc555f363ca10
tarantino19 <tolentinored19@gmail.com>
2022-10-16 18:14:35 +0800

    3.12 phonebook  - not done

---
+++ phonebookbe/mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://tolentinored19:${password}@phonebook.az1a1bi.mongodb.net/?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected')
+
+    const person = new Person({
+      content: 'HTML is Easy',
+      number: "043-1234464"
+    })
+
+    return person.save()
+  })
+  .then(() => {
+    console.log('person saved!')
+    return mongoose.connection.close()
+  })
+  .catch((err) => console.log(err))

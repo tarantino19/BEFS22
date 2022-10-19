1f1934e77a96bf961f2f8f3cbe050147174e9fb6
tarantino19 <tolentinored19@gmail.com>
2022-10-19 22:56:18 +0800

    3.15.3.18

--- phonebookbe/index.js
+++ phonebookbe/index.js
@@ -5,12 +5,6 @@ const cors = require('cors')
 require('dotenv').config()
 const Person = require('./models/person')
 
-
-app.use(express.json())
-app.use(cors())
-app.use(express.static('build'))
-
-
 morgan.token("data", (request) => {
   return request.method ? JSON.stringify(request.body) : " "; ;
 }); //this is the most basic one to show logger
@@ -18,6 +12,11 @@ morgan.token("data", (request) => {
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
+
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(person => {
@@ -33,32 +32,17 @@ app.get('/info', (request, response) => {
   response.send(responseInfo)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id)
-        .then((person) => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end();
-    }
-  })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = (request.params.id)
-  persons = persons.filter(person => person.id !== id)
   // for delete, persons being filtered has to be not equal to the id 
-  response.status(204).end()
-})
 
 //post request
-
-// const generateId = () => {
-//   const personId = persons.length > 0
-//     ? Math.max(...persons.map(n => n.id))
-//     : 0
-//   return personId + 1
-// }
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -77,12 +61,49 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id)
+        .then((person) => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end();
+    }
+  })
+  .catch(error => next(error))
+  })
+
+  app.put("/api/persons/:id", (request, response, next) => {
+    const { name, number } = request.body;
+    Person.findByIdAndUpdate(
+      request.params.id,
+      { name, number },
+    )
+      .then((updatedPerson) => {
+        response.json(updatedPerson);
+      })
+      .catch((error) => next(error));
+  });
+
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 // const unknownEndpoint = (request, response) => {
 //   response.status(404).send({ error: 'unknown endpoint' })
